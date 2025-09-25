f2a688d2bd15cd519696d7baba094077446fdbee
Philipe Morais <liperapltda@gmail.com>
2025-09-25 08:33:44 -0300

    initial commit

---
+++ apps/server/src/lib/auth.ts
@@ -0,0 +1,22 @@
+import { betterAuth, type BetterAuthOptions } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import { expo } from "@better-auth/expo";
+import prisma from "../db";
+
+export const auth = betterAuth<BetterAuthOptions>({
+	database: prismaAdapter(prisma, {
+		provider: "postgresql",
+	}),
+	trustedOrigins: [process.env.CORS_ORIGIN || "", "mybettertapp://", "exp://"],
+	emailAndPassword: {
+		enabled: true,
+	},
+	advanced: {
+		defaultCookieAttributes: {
+			sameSite: "none",
+			secure: true,
+			httpOnly: true,
+		},
+	},
+	plugins: [expo()],
+});
